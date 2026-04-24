d1eda835d9476a3df8611ff0855194f1de2c5d8b
Aditya Chaudhary <adityachaudhary7724@gmail.com>
2026-04-24 19:11:16 +0530

    fixed render port binding

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import re
+import os
 from collections import defaultdict
 
 app = Flask(__name__)
@@ -124,4 +125,5 @@ def bfhl():
 
 if __name__ == '__main__':
     print("🚀 Server starting...")
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
